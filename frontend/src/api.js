67479fc6e34ae525469c7c653500936fb1f2e126
morlayecisse1196 <morlayecis0003@gmail.com>
2025-11-22 00:32:05 +0000

    Fix Streamlit deployment: add requirements and runtime

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+
 const api = axios.create({
-  baseURL: "http://localhost:5000", // ton Flask backend
+//   baseURL: "http://localhost:5000", // ton Flask backend
+  baseURL: "https://projet-detection-poubelle.fly.dev", // ton Flask backend
+
 });
 
 export default api;
